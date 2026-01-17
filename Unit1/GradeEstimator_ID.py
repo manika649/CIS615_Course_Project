c7f44a467377882fa9a4be9068ba6392a163d3d3
manika649 <manikakarki1@gmail.com>
2026-01-17 12:11:32 -0600

    Create GradeEstimator_ID.py

---
+++ Unit1/GradeEstimator_ID.py
@@ -0,0 +1,8 @@
+# Create a variable for the first name and use input() to get text from the user's keyboard
+first_name = input("Please enter your first name: ") 
+
+# Create a variable for the last name and use input() to store the second piece of user data
+last_name = input("Please enter your last name: ") 
+
+# Use the print function to output both variables to the console with an automatic space between them
+print(first_name, last_name)
